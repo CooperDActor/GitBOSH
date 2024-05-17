160687447ef3e5a10259255cfa2fda5e53226809
Cooper D'Andilly <thesaltycdogwasatschool@outlook.com>
2024-05-17 17:03:16 +1000

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo crontab -e
+#sudo crontab -e
 #@reboot /home/pi/xmrig/build/xmrig -o gulf.moneroocean.stream:10128 -u 43FWtktcN1jD8jV5iaU9iR1vT9uvrPrHjR93Fpe4JGkUPHjtvvsNp2K1WUEfgaKNJXgk1GCTRQHfMWpibysocHweHMUp4pK -p SaltyCloud
 
 ./xmrig -o gulf.moneroocean.stream:10128 -u 43FWtktcN1jD8jV5iaU9iR1vT9uvrPrHjR93Fpe4JGkUPHjtvvsNp2K1WUEfgaKNJXgk1GCTRQHfMWpibysocHweHMUp4pK -p SaltyCloud
